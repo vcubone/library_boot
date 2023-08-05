e33459403524d664e3ff5e9a6cb1790bf951c2c2
vcubone <andrey.batorov@mai.ru>
2023-08-05 13:35:03 +0300

    add simple test

--- src/test/java/ru/batorov/library/LibraryApplicationTests.java
+++ src/test/java/ru/batorov/library/LibraryApplicationTests.java
@@ -1,13 +1,45 @@
 package ru.batorov.library;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+import ru.batorov.library.controllers.HomePageController;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class LibraryApplicationTests {
-
-	@Test
-	void contextLoads() {
+@AutoConfigureMockMvc
+@TestPropertySource("/application-test.properties")
+public class LibraryApplicationTests {
+	@Autowired
+    private MockMvc mvc;
+	@Autowired
+	private HomePageController homePageController;
+	
+	public LibraryApplicationTests() {
 	}
 
+	@Test
+	public void controllerExists() {
+		assertNotNull("no home page controller", homePageController);
+	}
+	
+	@Test
+	public void homePageWorking() throws Exception{
+		this.mvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("\u0414\u043E\u0431\u0440\u043E")));//Добро
+	}
 }
---
+++ src/test/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO `book`(`title`, `author`, `releaseYear`) VALUES ('titleT','authorT', 500);
